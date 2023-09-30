8144367d6c0cdcae0bbe69ab770e89b738587b7c
n0escape200 <n0_escape200@yahoo.com>
2023-10-01 00:52:06 +0300

    Finished a basic system for register and login

--- VehicleProject/backend/controller/auth.js
+++ VehicleProject/backend/controller/auth.js
@@ -1,10 +1,34 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
-export const register = async (req, res) => {
-  const newUser = new User(req.body);
+export const register = (req, res) => {
   try {
-    const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
+    bcrypt.hash(req.body.password, 12, async (err, hash) => {
+      const newUser = new User({ ...req.body, password: hash });
+      const savedUser = await newUser.save();
+      res.status(200).json(savedUser);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const users = await User.find();
+    users.map((user) => {
+      if (req.body.username == user.username) {
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+          if (result) {
+            res.status(200).json("User has been found");
+          } else {
+            res.status(409).json("Password incorrect");
+          }
+        });
+      } else {
+        res.status(404).json("User was not found");
+      }
+    });
   } catch (error) {
     res.status(500).json(error);
   }
