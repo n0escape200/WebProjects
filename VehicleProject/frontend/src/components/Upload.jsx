cd92b5e3448d5a29052d01d584b05b834f2b25ac
n0escape200 <n0_escape200@yahoo.com>
2023-10-11 01:24:47 +0300

    Finished the css for the custom file input

--- VehicleProject/frontend/src/components/Upload.jsx
+++ VehicleProject/frontend/src/components/Upload.jsx
@@ -1,18 +1,43 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./Upload.css";
 const Upload = () => {
   const fileRef = useRef(null);
+  const [photo, setPhoto] = useState();
+
   return (
     <div className="uploadContainer">
-      <div className="main"></div>
-      <input ref={fileRef} type="file" name="" id="" hidden />
       <div
         onClick={() => {
           fileRef.current.click();
         }}
         className="fileInput"
       >
-        Select file
+        <input
+          ref={fileRef}
+          type="file"
+          hidden
+          onChange={(event) => {
+            setPhoto(event.target.value);
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faPlus}
+          size="2xl"
+          style={{ color: "#ffffff" }}
+        />
+        <div
+          style={{
+            color: "white",
+            fontSize: 32,
+            letterSpacing: 1,
+            fontWeight: 600,
+            textShadow: "2px 2px 6px #000000",
+          }}
+        >
+          Add photos
+        </div>
       </div>
     </div>
   );
